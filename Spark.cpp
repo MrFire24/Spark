5e749625b8b05a2446fbb8e3d2d0af86d4313b3a
BuildTools <unconfigured@null.spigotmc.org>
2023-06-02 17:41:41 +0300

    Добавьте файлы проекта.

---
+++ Spark.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+#include "imgui.h"
+#include "imgui-SFML.h"
+#include <SFML/Graphics.hpp>
+
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(1200, 800), "Window Title");
+    ImGui::SFML::Init(window);
+
+    bool circleExists = true;
+    float circleRadius = 200.0f;
+    int circleSegments = 75;
+    float circleColor[3] = { (float)204 / 255, (float)77 / 255, (float)5 / 255 };
+    sf::CircleShape shape(circleRadius, circleSegments);
+    shape.setFillColor(sf::Color
+    (
+        (int)(circleColor[0] * 255),
+        (int)(circleColor[1] * 255),
+        (int)(circleColor[2] * 255)
+    )); // Color circle
+    shape.setOrigin(circleRadius, circleRadius);
+    shape.setPosition(window.getSize().x / 2, window.getSize().y / 2); // Center circle
+
+    sf::Clock deltaClock;
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            ImGui::SFML::ProcessEvent(event);
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        ImGui::SFML::Update(window, deltaClock.restart());
+
+        ImGui::Begin("Window title");
+        ImGui::Text("Window Pos = %d, %d", window.getPosition().x, window.getPosition().y);
+        ImGui::Checkbox("Circle", &circleExists);
+        ImGui::SliderFloat("Radius", &circleRadius, 100.0f, 300.0f);
+        ImGui::SliderInt("Sides", &circleSegments, 3, 100);
+        ImGui::ColorEdit3("Color Circle", circleColor);
+        ImGui::End();
+
+        shape.setRadius(circleRadius);
+        shape.setOrigin(circleRadius, circleRadius);
+        shape.setPointCount(circleSegments);
+        shape.setFillColor(sf::Color
+        (
+            (int)(circleColor[0] * 255),
+            (int)(circleColor[1] * 255),
+            (int)(circleColor[2] * 255)
+        )); // Color circle
+
+        window.clear(sf::Color(18, 33, 43)); // Color background
+        if (circleExists)
+            window.draw(shape);
+        ImGui::SFML::Render(window);
+        window.display();
+    }
+
+    ImGui::SFML::Shutdown();
+    return 0;
+}
