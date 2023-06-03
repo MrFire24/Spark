8fd9000af28cfb929a9f770b663d2a4d57e0cd87
BuildTools <unconfigured@null.spigotmc.org>
2023-06-03 14:25:04 +0300

    Добавил хитбоксы (в процессе) Добавил свой 3д вектор (vec3f) и файл vec3.cpp Поменял sf::Vector3f на vec3f Добавил вариант функции телепорта (с аргументом vec3f) Переместил инклюды из .cpp в .h

--- Backend.h
+++ Backend.h
@@ -1,5 +1,61 @@
 #pragma once
 
+#include "imgui.h"
+#include "imgui-SFML.h"
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <vector>
+#include <functional>
+
+class vec3f {
+public:
+	float x = 0, y = 0, z = 0;
+
+	vec3f(float x = 0, float y = 0, float z = 0);
+
+	void normalize();
+	vec3f get_norm();
+	float length();
+	void set(float x, float y, float z);
+
+	void operator+(vec3f vec);
+	void operator-(vec3f vec);
+
+	vec3f operator*(float num) const;
+	vec3f operator/(float num) const;
+
+	void operator=(vec3f vec);
+	void operator=(float num);
+
+	friend std::ostream& operator<<(std::ostream& stream, const vec3f& object) {
+		stream << object.x << " " << object.y << " " << object.z;
+		return stream;
+	}
+};
+
+
+/*
+class Hitbox {
+private:
+public:
+	bool isColliding(const Cylinder& cyl1, const Cylinder& cyl2);
+};
+
+class Cylinder : Hitbox {
+private:
+	float Radius_;
+	float Height_;
+	vec3f* Position_;
+	friend class Hitbox;
+public:
+	Cylinder(float radius, float height, vec3f* position) : Radius_(radius), Height_(height), Position_(position) {};
+};
+
+bool Hitbox::isColliding(const Cylinder& cyl1, const Cylinder& cyl2) {
+	//bool collXZ = 
+	bool collY = ((cyl1.Position_->y >= (cyl2.Position_->y - cyl2.Height_)) && ((cyl1.Position_->y - cyl1.Height_) <= cyl2.Position_->y));
+    return true;
+} */
 
 class Object{
 private:
@@ -7,7 +63,7 @@ private:
 	int ID_;	//Temporary
 	float Health_;
 	float Max_Health_;
-	sf::Vector3f Position_;
+	vec3f Position_;
 
 	//List Of Methods that must be Constantly Activated
 	std::vector<std::function<void()>> updatable_elements_;	//Not like this 
@@ -30,7 +86,7 @@ public:
 	int getID();
 	float getHealth();
 	float getMax_Health();
-	sf::Vector3f getPosition();
+    vec3f getPosition();
 
 	//Check Special Ñharacteristic
 	bool isImmortal();	//Max HP = -1 for immortal object
@@ -38,8 +94,10 @@ public:
 
 	//Actions
 	void kill(int);
+    void teleport(vec3f);
 	void teleport(float, float, float);
 	void teleport(float, float);
+
 
 	
 
